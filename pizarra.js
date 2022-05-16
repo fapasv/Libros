e0ee4687942eeab268642de406801fcf08e3ec9a
fapa <fapasv@gmail.com>
2022-05-16 15:31:54 -0600

    inicio de api

--- pizarra.js
+++ pizarra.js
@@ -1,15 +1,21 @@
 class Pizarra extends HTMLElement {
-    
+
     connectedCallback() {
-        this.accion ="<i class='fas fa-keyboard'></i>&nbsp;Digitar";
-        this.innerHTML = `<div class="signature-pad">
-            <div class="signature-pad--body">
-                <canvas class="border border-secondary" width="`+this.getAttribute("width")+`" height="`+this.getAttribute("height")+`"></canvas>
-                <textarea class="form-control" id="tbArea" rows="3" style="width:`+this.getAttribute("width")+`px; display:none"></textarea>
+
+        this.url = "https://localhost:44338/";
+
+        this.accion = "<i class='fas fa-keyboard'></i>&nbsp;Digitar";
+        this.innerHTML = `<div class="pizarra">
+        <div class="pizarra--header">
+        <h1 class="pizarra-titulo"></h1>
+        </div>
+            <div class="pizarra--body">
+                <canvas data-id="0" class="border border-secondary" width="`+ this.getAttribute("width") + `" height="` + this.getAttribute("height") + `"></canvas>
+                <textarea class="form-control" id="tbArea" rows="3" style="width:`+ this.getAttribute("width") + `px; display:none"></textarea>
             </div>            
         
-            <div class="signature-pad--footer">
-                <div class="signature-pad--actions">
+            <div class="pizarra--footer">
+                <div class="pizarra--actions">
                     <div class="btn-toolbar mb-3" role="toolbar">
                         <div class="btn-group me-2" role="group">
                             
@@ -23,54 +29,125 @@ class Pizarra extends HTMLElement {
                             
                         </div>
                         <div class="btn-group me-4" role="group">
-                            <button type="button" class="btn btn-sm btn-outline-secondary" data-action="change">`+this.accion+`</button>
+                            <button type="button" class="btn btn-sm btn-outline-secondary" data-action="change">`+ this.accion + `</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>`;
-        var libro =this.getAttribute("libro");
-        var usuario =this.getAttribute("usuario");
-        var ejercicio =this.getAttribute("ejercicio");
-        
+
+        this.canvas = this.querySelector("canvas");
+
         this.clearButton = this.querySelector("[data-action=clear]");
         this.undoButton = this.querySelector("[data-action=undo]");
         this.changeColorButton = this.querySelector("[data-action=change-color]");
         this.saveButton = this.querySelector("[data-action=save-data]");
 
-        var canvas = this.querySelector("canvas");
-        var signaturePad = new SignaturePad(canvas);
+        this.pizarra = ""
+        this.initCanvas();
 
-        this.saveButton.addEventListener("click", function (event) {
-            var data = signaturePad.toData();
+        this.saveButton.addEventListener("click", this.guardarData.bind(this));
 
-            if (data) {
-                alert(libro+" "+usuario+" "+ejercicio);
-                alert(data);
+        this.clearButton.addEventListener("click", this.limpiarCanvas.bind(this));
+
+        this.undoButton.addEventListener("click", this.deshacerTrazo.bind(this));
+
+        this.changeColorButton.addEventListener("click", this.cambiaColor.bind(this));
+
+        window.addEventListener('resize', this.reziseCanvas.bind(this));
+    }
+
+    guardarData() {
+
+        var usuario = this.getAttribute("usuario");
+        var ejercicio = this.getAttribute("ejercicio");
+
+        var data = this.pizarra.toData();
+        if (data) {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", this.url + "api/Ejercicio/" + ejercicio);
+
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4) {
+                    console.log(data);
+                }
+            };
+
+            xhr.send();
+        }
+    }
+
+    limpiarCanvas() {
+        this.pizarra.clear();
+    }
+
+    initCanvas() {
+        var usuario = this.getAttribute("usuario");
+        var ejercicio = this.getAttribute("ejercicio");
+        var titulo = this.querySelector(".pizarra-titulo");
+
+
+        this.pizarra = new SignaturePad(this.canvas);
+        this.reziseCanvas();
+        var c = this.canvas;
+        var p = this.pizarra;
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", this.url + "api/Ejercicio/ejercicios_usuario?id=" + ejercicio + "&userId=" + usuario);
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                var json = JSON.parse(xhr.responseText);
+                console.log(json);
+                titulo.innerHTML = json.enunciado;
+
+                if (json.respuestas.lenght > 0) {
+                    let data = json.respuestas[0];
+                    p.fromData(data.valor);
+                    c.setAttribute('data-id', data.id);
+                }
             }
-        });
+        };
 
-        this.clearButton.addEventListener("click", function (event) {
-            signaturePad.clear();
-        });
+        xhr.send();
 
-        this.undoButton.addEventListener("click", function (event) {
-            var data = signaturePad.toData();
 
-            if (data) {
-                data.pop(); // remove the last dot or line
-                signaturePad.fromData(data);
-            }
-        });
+    }
 
-        this.changeColorButton.addEventListener("click", function (event) {
-            var r = Math.round(Math.random() * 255);
-            var g = Math.round(Math.random() * 255);
-            var b = Math.round(Math.random() * 255);
-            var color = "rgb(" + r + "," + g + "," + b + ")";
+    reziseCanvas() {
+        // When zoomed out to less than 100%, for some very strange reason,
+        // some browsers report devicePixelRatio as less than 1
+        // and only part of the canvas is cleared then.
+        var ratio = Math.max(window.devicePixelRatio || 1, 1);
 
-            signaturePad.penColor = color;
-        });
+        // This part causes the canvas to be cleared
+        this.canvas.width = this.canvas.offsetWidth * ratio;
+        this.canvas.height = this.canvas.offsetHeight * ratio;
+        this.canvas.getContext("2d").scale(ratio, ratio);
+
+        // This library does not listen for canvas changes, so after the canvas is automatically
+        // cleared by the browser, SignaturePad#isEmpty might still return false, even though the
+        // canvas looks empty, because the internal data of this library wasn't cleared. To make sure
+        // that the state of this library is consistent with visual state of the canvas, you
+        // have to clear it manually.
+        this.pizarra.clear();
+    }
+
+    cambiaColor() {
+        var r = Math.round(Math.random() * 255);
+        var g = Math.round(Math.random() * 255);
+        var b = Math.round(Math.random() * 255);
+        var color = "rgb(" + r + "," + g + "," + b + ")";
+
+        this.pizarra.penColor = color;
+    }
+
+    deshacerTrazo() {
+        var data = this.pizarra.toData();
+
+        if (data) {
+            data.pop(); // remove the last dot or line
+            this.pizarra.fromData(data);
+        }
     }
 
     disconnectedCallback() {
@@ -78,9 +155,6 @@ class Pizarra extends HTMLElement {
         this.undoButton.removeEventListener("click", this);
         this.changeColorButton.removeEventListener("click", this);
         this.saveButton.removeEventListener("click", this);
-      }
-
-   
-   
+    }
 }
-customElements.define('fa-pizarra',Pizarra);
+customElements.define('fa-pizarra', Pizarra);
