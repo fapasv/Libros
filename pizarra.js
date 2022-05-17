3a329721287c1aca047aa89dd47dec1b2368b4d7
Farid Aldana <faldana@salud.gob.sv>
2022-05-16 20:08:26 -0600

    estilo y funcionalidad

--- pizarra.js
+++ pizarra.js
@@ -1,11 +1,15 @@
 class Pizarra extends HTMLElement {
-    
-    connectedCallback() {
-        this.accion ="<i class='fas fa-keyboard'></i>&nbsp;Digitar";
-        this.innerHTML = `<div class="signature-pad">
+  constructor() {
+    super();
+
+    this.accion = "<i class='fas fa-keyboard'></i>&nbsp;Digitar";
+    this.innerHTML =
+      `<div class="signature-pad">
             <div class="signature-pad--body">
-                <canvas class="border border-secondary" width="`+this.getAttribute("width")+`" height="`+this.getAttribute("height")+`"></canvas>
-                <textarea class="form-control" id="tbArea" rows="3" style="width:`+this.getAttribute("width")+`px; display:none"></textarea>
+                <canvas class="border border-secondary"></canvas>
+                <textarea class="form-control" id="tbArea" rows="3" style="width:` +
+      this.getAttribute("width") +
+      `px; display:none"></textarea>
             </div>            
         
             <div class="signature-pad--footer">
@@ -23,64 +27,90 @@ class Pizarra extends HTMLElement {
                             
                         </div>
                         <div class="btn-group me-4" role="group">
-                            <button type="button" class="btn btn-sm btn-outline-secondary" data-action="change">`+this.accion+`</button>
+                            <button type="button" class="btn btn-sm btn-outline-secondary" data-action="change">` +
+      this.accion +
+      `</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>`;
-        var libro =this.getAttribute("libro");
-        var usuario =this.getAttribute("usuario");
-        var ejercicio =this.getAttribute("ejercicio");
-        
-        this.clearButton = this.querySelector("[data-action=clear]");
-        this.undoButton = this.querySelector("[data-action=undo]");
-        this.changeColorButton = this.querySelector("[data-action=change-color]");
-        this.saveButton = this.querySelector("[data-action=save-data]");
 
-        var canvas = this.querySelector("canvas");
-        var signaturePad = new SignaturePad(canvas);
+    //
+  }
+  connectedCallback() {
+    this.canvas = this.querySelector("canvas");
+    this.signaturePad = new SignaturePad(this.canvas);
 
-        this.saveButton.addEventListener("click", function (event) {
-            var data = signaturePad.toData();
+    
 
-            if (data) {
-                alert(libro+" "+usuario+" "+ejercicio);
-                alert(data);
-            }
-        });
+    this.clearButton = this.querySelector("[data-action=clear]");
+    this.undoButton = this.querySelector("[data-action=undo]");
+    this.changeColorButton = this.querySelector("[data-action=change-color]");
+    this.saveButton = this.querySelector("[data-action=save-data]");
 
-        this.clearButton.addEventListener("click", function (event) {
-            signaturePad.clear();
-        });
+    this.saveButton.addEventListener("click", this.guardarCanvas.bind(this));
 
-        this.undoButton.addEventListener("click", function (event) {
-            var data = signaturePad.toData();
+    this.clearButton.addEventListener("click", this.limpiarCanvas.bind(this));
 
-            if (data) {
-                data.pop(); // remove the last dot or line
-                signaturePad.fromData(data);
-            }
-        });
+    this.undoButton.addEventListener("click", this.deshacerEnCanvas.bind(this));
 
-        this.changeColorButton.addEventListener("click", function (event) {
-            var r = Math.round(Math.random() * 255);
-            var g = Math.round(Math.random() * 255);
-            var b = Math.round(Math.random() * 255);
-            var color = "rgb(" + r + "," + g + "," + b + ")";
+    this.changeColorButton.addEventListener(
+      "click",
+      this.colorearCanvas.bind(this)
+    );
 
-            signaturePad.penColor = color;
-        });
+    window.addEventListener("resize", this.resizeCanvas.bind(this));
+  }
+
+  colorearCanvas() {
+    var r = Math.round(Math.random() * 255);
+    var g = Math.round(Math.random() * 255);
+    var b = Math.round(Math.random() * 255);
+    var color = "rgb(" + r + "," + g + "," + b + ")";
+
+    this.signaturePad.penColor = color;
+  }
+  deshacerEnCanvas() {
+    var data = this.signaturePad.toData();
+
+    if (data) {
+      data.pop(); // remove the last dot or line
+      this.signaturePad.fromData(data);
     }
+  }
+  guardarCanvas() {
+    var libro = this.getAttribute("libro");
+    var usuario = this.getAttribute("usuario");
+    var ejercicio = this.getAttribute("ejercicio");
 
-    disconnectedCallback() {
-        this.clearButton.removeEventListener("click", this);
-        this.undoButton.removeEventListener("click", this);
-        this.changeColorButton.removeEventListener("click", this);
-        this.saveButton.removeEventListener("click", this);
-      }
+    var data = this.signaturePad.toData();
+    if (data) {
+      alert(libro + " " + usuario + " " + ejercicio);
+      alert(data);
+    }
+  }
 
-   
-   
+  limpiarCanvas() {
+    this.signaturePad.clear();
+  }
+
+  resizeCanvas() {
+    alert("puto");
+    var ratio = Math.max(window.devicePixelRatio || 1, 1);
+    this.canvas.width = this.canvas.offsetWidth * ratio;
+    this.canvas.height = this.canvas.offsetHeight * ratio;
+    this.canvas.getContext("2d").scale(ratio, ratio);
+
+    this.signaturePad.clear();
+  }
+
+  disconnectedCallback() {
+    this.clearButton.removeEventListener("click", this.limpiarCanvas);
+    this.undoButton.removeEventListener("click", this.deshacerEnCanvas);
+    this.changeColorButton.removeEventListener("click", this.colorearCanvas);
+    this.saveButton.removeEventListener("click", this.guardarCanvas);
+    signaturePad.off();
+  }
 }
-customElements.define('fa-pizarra',Pizarra);
+customElements.define("fa-pizarra", Pizarra);
